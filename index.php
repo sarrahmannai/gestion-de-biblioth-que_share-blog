538e030e255e393308291d09080c7268faf7dedb
sarrahmannai <38835699+sarrahmannai@users.noreply.github.com>
2018-05-09 22:11:17 +0200

    nouveaux

--- index.php
+++ index.php
@@ -4,90 +4,14 @@ Author URL: http://w3layouts.com
 License: Creative Commons Attribution 3.0 Unported
 License URL: http://creativecommons.org/licenses/by/3.0/
 -->
+
 <?php get_header(); ?>
-
-<!--------------------- Main Content ------------------->
-<div class="wrap">
-	<div class="main">
-		<div class="content">
-			<div class="box1">
-   				 <h3><a href="single.html">Making it look like readable English. Many desktop packages and web page</a></h3>
-    				<span>By Kieth Deviec- 2 hours ago<span class="comments">8 Comments</span></span> 
-			   <div class="view">
-					<div class="view-back">
-						<span class="views" title="views">(566)</span>
-						<span class="likes" title="likes">(124)</span>
-						<span class="link" title="link">(24)</span>
-						<a href="single.html"> </a>
-					</div>
-	<a href="single.html"><img src="images/img1.jpg"></a>
-				</div>
-				<div class="data">
-				    <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editorsLorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors</p>
-				    <span><a href="single.html">Continue reading >>></a></span>
-				</div>
-			<div class="clear"></div>
-		</div>
-			<div class="box1">
-			   	 <h3><a href="single.html">Making it look like readable English. Many desktop packages and web page</a></h3>
-			    <span>By Fred G.Meyer- 10 hours ago<span class="comments">5 Comments</span></span>
-			 <div class="view">
-					<div class="view-back">
-						<span class="views" title="views">(566)</span>
-						<span class="likes" title="likes">(124)</span>
-						<span class="link" title="link">(24)</span>
-						<a href="single.html"> </a>
-					</div>
-					<a href="single.html"><img src="images/img2.jpg"></a>
-				</div>
-				<div class="data">
-				    <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editorsLorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors</p>
-				    <span><a href="single.html">Continue reading >>></a></span>
-				</div>
-			<div class="clear"></div>
-	     </div>
-				<div class="box1">
-				    <h3><a href="single.html">Making it look like readable English. Many desktop packages and web page</a></h3>
-				    <span>By R.J Cliton - 1 day ago<span class="comments">12 Comments</span></span>
-				 <div class="view">
-					<div class="view-back">
-						<span class="views" title="views">(566)</span>
-						<span class="likes" title="likes">(124)</span>
-						<span class="link" title="link">(24)</span>
-						<a href="single.html"> </a>
-					</div>
-					<a href="single.html"><img src="images/img3.jpg"></a>
-				</div>
-					<div class="data">
-					    <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editorsLorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors</p>
-					    <span><a href="single.html">Continue reading >>></a></span>
-					</div> 
-				<div class="clear"></div>
-			</div>
-					<div class="page_links">
-						<div class="next_button">
-							 <a href="#">Next</a>
-						</div>
-							<div class="page_numbers">
-							    <ul>
-									<li><a href="#">1</a>
-									<li class="active"><a href="#">2</a>
-									<li><a href="#">3</a>
-									<li><a href="#">4</a>
-									<li><a href="#">5</a>
-									<li><a href="#">6</a>
-									<li><a href="#">... Next</a>
-								</ul>
-							</div>	
-						<div class="clear"></div>
-					<div class="page_bottom">
-						<p>Back To : <a href="#">Top</a> |  <a href="#">Home</a></p>
-					</div>
-			</div>
-</div>
-<?php get_sidebar(); ?>
-
-
-
+<div class="row">
+ <div class="col-sm-8 blog-main">
+ <?php get_template_part( 'content', get_post_format() ); ?>
+ </div> <!-- /.blog-main -->
+ <?php get_sidebar(); ?>
+</div> <!-- /.row -->
 <?php get_footer(); ?>
-<!--------------------- End Main Content ----------------------------------->
+
+
